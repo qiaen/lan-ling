908ce5cf33c3692b5c686b2cc80fd95083f3c735
刘洪亮 <hongliang.liu@zhangmen.com>
2021-03-27 17:14:54 +0800

    # 优化路由功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,6 @@
-import layout from '@/views/layout/index.vue'
+// import layout from '@/views/layout/index.vue'
 Vue.use(VueRouter)
 import routes from './routes'
 export default new VueRouter({
-	routes: [{
-		path: '/',
-		name: '首页',
-		component: layout,
-		meta: {
-			title: '首页'
-		},
-		children: [{
-			path: '',
-			name: 'Dashboard',
-			component: () => import('@views/daily/index.vue'),
-			meta: {
-				title: 'dashboard',
-				icon: 'iconfont icon-dashboard'
-			}
-		}]
-	}, {
-		path: '/login',
-		name: '登录',
-		component: () => import('@views/login/'),
-		meta: {
-			icon: 'el-icon-s-platform',
-			title: 'login'
-		}
-	}]
+	routes: routes
 })
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,10 @@
 // 注意，name要和组件内的name一致，用于路由缓存识别！！！
 // disCache：true配置后，禁用页面缓存
+import layout from '@/views/layout/index.vue'
 export default [{
 	path: '/',
 	name: '首页',
-	component: import('@/views/layout/index.vue'),
+	component: layout,
 	meta: {
 		title: '首页'
 	},
